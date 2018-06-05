a8bc29c8c3967fefd067015f440a6ce03b85b654
AndreasMN <andycutter@godmail.dk>
2018-06-05 13:55:29 +0200

    Typos

--- spec/spec.helper.js
+++ spec/spec.helper.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 
 let calc = fs.readFileSync('./src/calculator.js');
 let person = fs.readFileSync('./src/person.js');
-eval( person + `\nexports.Person = Person;\nexports.BMICalculator = BMICalculator;`)
+eval(calc + person + `\nexports.Person = Person;\nexports.BMICalculator = BMICalculator;`)
